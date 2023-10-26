ff6f160722334f56729517406aecd42a84605175
Marc S. Brooks <marc@mbrooks.info>
2023-10-26 12:47:47 -0700

    Added pseudo-random string generator

--- src/Utils.js
+++ src/Utils.js
@@ -4,6 +4,42 @@
 const Utils = this;
 
 /**
+ * Generate a pseudo-random string.
+ *
+ * @param {Number} len
+ *   Character length (default: 32).
+ *
+ * @return {String|undefined}
+ *
+ * @example
+ * consr result = Utils.genRandomStr(10);
+ */
+exports.genRandomStr = function(len = 32) {
+  let chars = 'abcdefghjkmnopqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ23456789'
+    .split('');
+
+  // Perform (Durstenfeld) shuffle.
+  for (let i = chars.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const tmp = chars[i];
+
+    chars[i] = chars[j];
+    chars[j] = tmp;
+  }
+
+  chars = chars.join('');
+
+  let str = '';
+
+  // Trim string to length.
+  for (let i = 0; i < len; i++) {
+    str += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+
+  return str || undefined;
+};
+
+/**
  * Return Pascal Case formatted string.
  *
  * @param {String} valueChanged(newValue) {
--- test/unit/Utils.js
+++ test/unit/Utils.js
@@ -9,6 +9,21 @@ const Utils = require(`${PACKAGE_ROOT}/src/Utils.js`);
 
 describe('Utils module', function() {
   describe('Static methods', function() {
+    describe('genRandomStr', function() {
+      const result1 = Utils.genRandomStr();
+      const result2 = Utils.genRandomStr(16);
+      const result3 = Utils.genRandomStr('foo');
+
+      it('should return value', function() {
+        expect(result1).to.be.a('string').to.have.lengthOf(32);
+        expect(result2).to.be.a('string').to.have.lengthOf(16);
+      });
+
+      it('should not return value', function() {
+        expect(result3).to.be.undefined;
+      });
+    });
+
     describe('pascalCase', function() {
       const result1 = Utils.pascalCase('foo-bar*Biz_baz');
       const result2 = Utils.pascalCase('FOO_BAR_BIZ_BAZ');
